0f0ae41ffd10ed399ca47361dbc27ae809a8fc9b
Illias Askew <wiaskew1@gmail.com>
2021-01-26 19:06:01 -0500

    lab1

---
+++ lab1/Illias.js
@@ -0,0 +1,7 @@
+import Person from './Person.js';
+
+class Illias extends Person{
+    constructor(){
+        super("Illias", '#FF285D');
+    }
+}
---
+++ lab1/index.js
@@ -0,0 +1,4 @@
+import Person from './Person.js';
+
+const p = new Person ('Illias', '#FF285D');
+p.speak();
